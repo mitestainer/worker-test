30794f28eede3f702f31367b18edb37af267c2a8
Matheus Mitestainer <37314933+mitestainer@users.noreply.github.com>
2022-05-23 10:39:30 -0300

    add tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
+      {/* <Script
         strategy="worker"
         src="https://www.googletagmanager.com/gtag/js?id=G-PKJ7F2ESKE"
       />
@@ -18,6 +18,19 @@ function MyApp({ Component, pageProps }) {
             gtag('js', new Date());
 
             gtag('config', 'G-PKJ7F2ESKE');
+  `,
+        }}
+      /> */}
+      <Script
+        id="gtm-tag"
+        strategy="worker"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MNCK5KR');
   `,
         }}
       />
